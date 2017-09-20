00b75453f1b1481432d92f42633e5ed8e4afaed9
handsomeware <handsomeware1@gmail.com>
2017-09-20 15:34:05 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include "meta.php";?>
+<style>
+	body{
+		color: #800000;
+		background-color: #000000;
+		background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtVXafR_bDXjLdr_PeXn1Kxrv96zEHdMKzrAUgLRejiWi_9_TL);
+ 		background-repeat:no-repeat;
+ 		background-attachment: fixed;
+ 		background-size: 1000px;
+ 		background-position:left;
+		margin: 50px auto;
+		font-weight: thin;
+		font-family: 'Electrolize';
+     Width: 450px ';
+     }
+</style>
+</head>
+</html>
